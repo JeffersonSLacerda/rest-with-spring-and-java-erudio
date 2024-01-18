e6d8218e106b9a96276009439031d1ea8d887daf
JeffersonSLacerda <jefferson.lacerdasi@outlook.com>
2024-01-18 17:27:15 -0300

    First Commit

---
+++ rest-with-spring-and-java-erudio/src/test/java/com/lacerdadev/restwithspringandjavaerudio/StartUp.java
@@ -0,0 +1,13 @@
+package com.lacerdadev.restwithspringandjavaerudio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartUp {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
